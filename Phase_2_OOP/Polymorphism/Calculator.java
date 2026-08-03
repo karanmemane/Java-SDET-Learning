3c974eb9ab74a3784e127570941599f56d5642ac
karanmemane <karanmemane2020@gmail.com>
2026-08-03 20:39:01 +0530

    Revision of OOPs Concepts

--- Phase_2_OOP/Polymorphism/Calculator.java
+++ Phase_2_OOP/Polymorphism/Calculator.java
@@ -1,4 +1,5 @@
 package Polymorphism;
+//Polymorphism means “many forms.” In Java, it lets one name represent multiple behaviors.
 public class Calculator {
 
     // Method 1: Takes two integers
@@ -15,7 +16,24 @@ public class Calculator {
     double add(double a, double b) {
         return a + b;
     }
+ /*The class has multiple add(...) methods.
+Each method has the same name but different parameter lists:
+add(int, int)
+add(int, int, int)
+add(double, double)
+This is called compile-time polymorphism (method overloading).
 
+Key idea
+The same method name can work with different inputs.
+The compiler chooses the right version based on the arguments. */
+    class add extends Calculator {
+        // Method 4: Same method name, but different parameter types
+        @override
+        String add(String a, String b) { // method overloading with different parameter types   
+            return a + b;
+        }
+
+    }
 
     public static void main(String[] args) {
 
