6a25520de924aa7e29622caeb5dc778c5ed043d0
karanmemane <karanmemane2020@gmail.com>
2026-08-09 11:39:01 +0530

    Updated version of calculator

--- Calculator/calculator.java
+++ Calculator/calculator.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 class Calculator {
 
     public static void main(String[] args) {
-
+        System.out.println("Welcome to My Calculator!");
         ArrayList<Integer> numbers = new ArrayList<>();
         Scanner scanner = new Scanner(System.in);
 
