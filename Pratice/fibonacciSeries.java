67ce3d60ab30f3a729e02960e02a1e372fdd3f62
karanmemane <karanmemane2020@gmail.com>
2026-08-27 21:05:08 +0530

    Fibonacci series

---
+++ Pratice/fibonacciSeries.java
@@ -0,0 +1,21 @@
+package Pratice;
+public class fibonacciSeries {
+
+    public static void main(String[] args) {
+
+        int n = 10;
+        int first = 0;
+        int second = 1;
+
+        System.out.println("Fibonacci Series:");
+
+        for (int i = 1; i <= n; i++) {
+
+            System.out.print(first + " ");
+
+            int next = first + second;
+            first = second;
+            second = next;
+        }
+    }
+}
